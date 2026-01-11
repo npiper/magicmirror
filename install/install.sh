4a94257e5fb6c146de7b6a9251451f2985f61c77
Karsten Hassel <hassel@gmx.de>
2026-01-11 16:38:57 +0100

    update init script, update install.sh

--- install/install.sh
+++ install/install.sh
@@ -41,7 +41,7 @@ fi
 _info "--> Check docker installation"
 
 if command -v docker > /dev/null; then
-  echo "docker is already installed:"
+  echo "Docker is already installed:"
   docker -v
   docker compose version
 else
@@ -56,7 +56,7 @@ if [[ -d "/run/systemd" && "$_uid" == "0" ]]; then
     if [[ -f "/run/systemd/timesync/synchronized" ]]; then
       break
     fi
-    [[ i -eq 1 ]] && echo "waiting for timesync " || echo -n "."
+    [[ i -eq 1 ]] && echo "Waiting for timesync " || echo -n "."
     sleep 5s
   done
 fi
@@ -74,39 +74,40 @@ else
   cp original.compose.yaml compose.yaml
 fi
 
-if [[ -f ".env" ]]; then
-  # cleanup old setups, remove later ...
-  sed -i 's|^LAB_WC_SOCK_DIR="${XDG_RUNTIME_DIR:-/tmp/labwc}"|LAB_WC_SOCK_DIR="/tmp/labwc"|g' .env
-  sed -i 's|^XDG_RUNTIME_DIR="${XDG_RUNTIME_DIR:-/run/user/$MM_UID}"|LAB_WC_SOCK_DIR="/tmp/labwc"|g' .env
-else
+if [[ ! -f ".env" ]]; then
   _info "--> Magicmirror container setup for scenario $scenario"
 
-  echo "creating .env file"
+  echo "Creating .env file"
 
   cp original.env .env
 
   # if not on master use develop image
   if [[ "$branch" != "master" ]]; then
-    echo "we are not on the master branch so using image karsten13/magicmirror:develop"
+    echo "We are not on the master branch so using image karsten13/magicmirror:develop"
     sed -i 's|MM_IMAGE=.*|MM_IMAGE="karsten13/magicmirror:develop"|g' .env
-    sed -i 's|LABWC_IMAGE=.*|LABWC_IMAGE="karsten13/labwc:develop"|g' .env
   fi
+fi
 
-  # set scenario:
-  sed -i 's|MM_SCENARIO=.*|MM_SCENARIO="'"$scenario"'"|g' .env
-  if [[ "$scenario" == "client" ]]; then
-    sed -i 's|MM_INIT=.*|MM_INIT="no"|g' .env
+# set properties depending on scenario:
+sed -i 's|MM_SCENARIO=.*|MM_SCENARIO="'"$scenario"'"|g' .env
+if [[ "$scenario" == "client" ]]; then
+  sed -i 's|MM_INIT=.*|MM_INIT="no"|g' .env
+else
+  sed -i 's|MM_INIT=.*|MM_INIT="init"|g' .env
+fi
+
+sed -i 's|MM_LABWC=.*|MM_LABWC="no"|g' .env
+if [[ "$scenario" != "server" ]]; then
+  if ! xset -q > /dev/null 2>&1 && ! command -v labwc > /dev/null 2>&1; then
+    # use own labwc
+    echo "Found no xserver or wayland so using own container"
+    sed -i 's|MM_LABWC=.*|MM_LABWC="labwc"|g' .env
+    # cleanup old setups, remove later ...
+    sed -i -r 's|^(XDG_RUNTIME_DIR=.*)"|# \1|g' .env
+    sed -i 's|^LAB_WC_SOCK_DIR=.*"|LAB_WC_SOCK_DIR="/tmp/labwc"|g' .env
   else
-    sed -i 's|MM_INIT=.*|MM_INIT="init"|g' .env
-  fi
-
-  sed -i 's|MM_LABWC=.*|MM_LABWC="no"|g' .env
-  if [[ "$scenario" == "electron" ]]; then
-    if ! xset -q > /dev/null 2>&1 && ! command -v labwc > /dev/null 2>&1; then
-      # use own labwc
-      echo "found no xserver or wayland so using own container"
-      sed -i 's|MM_LABWC=.*|MM_LABWC="labwc"|g' .env
-    fi
+    # use xserver/wayland from host
+    sed -i 's|^LAB_WC_SOCK_DIR=.*"|LAB_WC_SOCK_DIR="${XDG_RUNTIME_DIR:-/tmp/labwc"|g' .env
   fi
 fi
 
